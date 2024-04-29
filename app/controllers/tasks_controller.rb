b96a3297ac1cf629df281f2f5039127e3e57bc7c
Grant Davis <grantdavis303@gmail.com>
2024-04-28 21:52:23 -0600

    Add Task Show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,20 @@
 class TasksController < ApplicationController
   def index
-    @tasks = ["Task 1", "Task 2", "Task 3"]
+    @tasks = Task.all
   end
 
   def new
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
   def create
-    task = Task.new({
+    task = Task.create!({
       title: params[:title],
       description: params[:description]
     })
-
-    task.save
 
     redirect_to tasks_path
   end
