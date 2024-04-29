b62c7f0170ce23733866eb4a2033afdea3fabf6a
Grant Davis <grantdavis303@gmail.com>
2024-04-28 22:24:47 -0600

    Add Seed Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,6 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Seeds
+
+@task_1 = Task.create!(title: "Get a Haircut", description: "Your hair is getting long - time for a cut!")
+@task_2 = Task.create!(title: "Do Laundry", description: "You have no clean clothes - time to wash some!")
+@task_3 = Task.create!(title: "Brush Teeth", description: "Important for your dental health")
+@task_4 = Task.create!(title: "Get Groceries", description: "What are you going to eat with no food in the house?")
