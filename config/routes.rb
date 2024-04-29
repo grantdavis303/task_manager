d9246a42993385e1ff801a1a89a9a7c91a2f6e6f
Grant Davis <grantdavis303@gmail.com>
2024-04-28 21:40:59 -0600

    Format Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
 
   root to: "welcome#index"
 
-  get "/tasks", to: "tasks#index"
-  get "/tasks/new", to: "tasks#new"
-  post "/tasks", to: "tasks#create"
+  resources :tasks
 
 end
